8442e194c69cfaebb1e018090588939c7676c8f5
MariusSimkonis <113015789+MariusSimkonis@users.noreply.github.com>
2022-10-30 21:05:29 +0200

    Add files via upload

---
+++ mano_lib.hpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+#include <cmath>
+#include <iomanip>
+#include <vector>
+
+using std::cout;
+using std::cin;
+using std::endl;
+using std::setprecision;
+using std::fixed;
+using std::setw;
+using std::left;
+using std::right;
+using std::vector;
+using std::string;
+//using std::sort;
